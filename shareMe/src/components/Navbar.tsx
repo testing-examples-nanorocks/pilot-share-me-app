234f9ebb192b9e03d323b72bcb2da96b8f44b31a
nanorocks <andrejnankov@gmail.com>
2022-04-19 00:48:12 +0200

    Add index

--- shareMe/src/components/Navbar.tsx
+++ shareMe/src/components/Navbar.tsx
@@ -113,7 +113,7 @@ export default function Navbar(props: Props) {
                   </div>
 
                   <div
-                    className={`${!dropdown ? 'hidden' : ''} origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
+                    className={`${!dropdown ? 'hidden' : ''} z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
                     role="menu"
                     aria-orientation="vertical"
                     aria-labelledby="user-menu-button"
