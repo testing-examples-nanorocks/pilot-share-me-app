077e13051024c250aa9ba29ad9a0f2220499b0fe
nanorocks <andrejnankov@gmail.com>
2022-04-19 00:53:15 +0200

    Add missing inputs

--- shareMe/src/pages/ProfilePage.tsx
+++ shareMe/src/pages/ProfilePage.tsx
@@ -29,6 +29,24 @@ export default function Profile({ }: Props) {
                       <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
                         <div className="grid grid-cols-3 gap-6">
                           <div className="col-span-3 sm:col-span-2">
+                            <label htmlFor="company-website" className="block text-sm font-medium text-gray-700"> Full name </label>
+                            <div className="mt-1 flex rounded-md shadow-sm">
+                              <input type="text" name="company-website" id="company-website" className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded sm:text-sm border-gray-300" placeholder="Andrej Nankov" />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="grid grid-cols-3 gap-6">
+                          <div className="col-span-3 sm:col-span-2">
+                            <label htmlFor="company-website" className="block text-sm font-medium text-gray-700"> Email </label>
+                            <div className="mt-1 flex rounded-md shadow-sm">
+                              <input type="text" name="company-website" id="company-website" className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded sm:text-sm border-gray-300" placeholder="mail@gmail.com" disabled />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="grid grid-cols-3 gap-6">
+                          <div className="col-span-3 sm:col-span-2">
                             <label htmlFor="company-website" className="block text-sm font-medium text-gray-700"> Website </label>
                             <div className="mt-1 flex rounded-md shadow-sm">
                               <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> http:// </span>
